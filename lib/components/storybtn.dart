ac509e1d3cd4f3785a084f57b0cc2e7e498174d1
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2022-06-13 02:39:42 +0700

    Custom widget was created - storybtn.dart

--- lib/components/storybtn.dart
+++ lib/components/storybtn.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
 
-// Widget story
+Widget storyButton(String imgUrl, String username)
+{
+  return Column(
+    children: [
+      CircleAvatar(
+        backgroundImage: NetworkImage(imgUrl),
+        radius: 20.0,
+      ),
+      SizedBox(height: 20.0,),
+      Text(username, style: TextStyle(color: Colors.white),)
+    ],
+  );
+}
